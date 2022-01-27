35bd2ce0c9a57ed57ca2c80a202459638772a90c
Lidan-Huang <huangld0314@gmail.com>
2022-01-26 20:36:25 -0800

    finished basic one

--- app.py
+++ app.py
@@ -7,3 +7,23 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.get('/')
+def show_form():
+    """ 
+    Takes prompts from the story instance, updates questions
+    html form to reflect prompts. When submitted makes a GET 
+    request with form values to the flask show_story 
+    """
+    
+    return render_template("questions.html", prompts = silly_story.prompts)
+
+
+@app.get('/story')
+def show_story():
+    """ 
+    Accepts dictionary-like object from show_form GET 
+    request, calls generate method to produce story text.
+    """
+
+    return render_template("story.html", text=silly_story.generate(request.args))
